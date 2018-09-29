62005607835a00e5013cbd44c808b82ec4941022
Splerge <s3539519@student.rmit.edu.au>
2018-09-30 00:19:31 +1000

    Added tasks 3 and 4, began working on 5

--- assignment.sh
+++ assignment.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+
+QUIT=0
+LEDLIST=()
+
+
 print_menu(){
 	ledlist=("$@")
 	echo ""
@@ -17,10 +22,58 @@ print_menu(){
 
 }
 
+trigger_menu(){
+	QUIT_LED_MENU=0
+	while [ $QUIT_LED_MENU -eq 0 ] ; do 
+		echo "========================"
+		echo "Available events are:"
+		event_array=( `cat "/sys/class/leds/$1/trigger" `)
+		option=0
+		for i in "${event_array[@]}"
+		do
+			echo "${option}) $i"
+			((option++))
+		done
+		echo "${option}) Quit"
+		read CHOICE
+	done
+	
+}
+
+led_menu(){
+	QUIT_LED_MENU=0
+	while [ $QUIT_LED_MENU -eq 0 ] ; do 
+		echo ""
+		echo $1
+		echo "========================"
+		echo "What would you like to do with this LED:"
+		echo "1) Turn on"
+		echo "2) Turn off"
+		echo "3) Associate with a system event"
+		echo "4) Associate with a process' performance"
+		echo "5) Stop association with a process' performance"
+		echo "6) Quit"
+		echo "Please enter a number for your choice:"
+		read CHOICE
+		if [[ $CHOICE = 1 ]] ; then
+			echo "1" >/sys/class/leds/$1/brightness
+		fi
+
+		if [[ $CHOICE = 2 ]] ; then
+			echo "0" >/sys/class/leds/$1/brightness
+		fi
+
+		if [[ $CHOICE = 3 ]] ; then
+			trigger_menu $1
+		fi
+
+		if [[ $CHOICE = 6 ]] ; then
+			break
+		fi
+	done
+}
 
 #Program entry point
-QUIT=0
-LEDLIST=()
 
 #get the dir
 for dir in /sys/class/leds/*/; do
@@ -37,7 +90,7 @@ while [ $QUIT -eq 0 ] ; do
 
 	for i in ${!LEDLIST[@]}; do
 		if [[ $i = $CHOICE ]] ; then
-			echo ${LEDLIST[$i]}
+			led_menu ${LEDLIST[$i]}
 		fi
 	done
 done
