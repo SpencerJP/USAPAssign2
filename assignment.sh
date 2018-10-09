69b4914ac963d3b36d42806e56ecce159f2032e6
Splerge <s3539519@student.rmit.edu.au>
2018-10-09 21:35:06 +1100

    Ran shellcheck, and fixed all issues it came up with.

--- assignment.sh
+++ assignment.sh
@@ -12,9 +12,9 @@ print_menu(){
 	echo "========================"
 	echo "Please select a led to configure:"
 	option=0
-	for i in "${ledlist[@]}";
+	for led in "${ledlist[@]}";
 	do
-		echo "${option}) $i"
+		echo "${option}) $led"
 		((option++))
 	done
 	echo "${option}) Quit"
@@ -25,35 +25,36 @@ print_menu(){
 
 #this function prints the menu for editing the triggers.
 trigger_menu(){
-	QUIT_LED_MENU=0 #exit variable
-	while [ $QUIT_LED_MENU -eq 0 ] ; do 
+	QUIT_TRIGGER_MENU=0 #exit variable
+	while [ $QUIT_TRIGGER_MENU -eq 0 ] ; do 
 		echo "========================"
 		echo "Available events are (enter to scroll, get to bottom to begin input):"
-		event_array=( `cat "/sys/class/leds/$1/trigger" `)
+		IFS=" " read -r -a event_array <<< "$(cat /sys/class/leds/"${1}"/trigger)" #load trigger file into array
 		string_to_pipe="";
 		option=0
-		for i in "${event_array[@]}"
+		for trigger in "${event_array[@]}"
 		do
-			string_to_pipe="${string_to_pipe} ${option}) $i\n"
+			string_to_pipe="${string_to_pipe} ${option}) ${trigger} \\n"
 			((option++))
 		done
 		string_to_pipe="${string_to_pipe} ${option}) Quit"
-		printf "${string_to_pipe}" | more #pipe into more for pagination
+		printf "%b" "${string_to_pipe}" | more #pipe into more for pagination, %b allows it to process newlines
 
-		read CHOICE
+		read -r CHOICE
 		option=0
-		for i in "${event_array[@]}"
+		for trigger in "${event_array[@]}"
 		do
-			if [[ $option = $CHOICE ]] ; then
+			if [[ $option = "${CHOICE}" ]] ; then
 				#using tee to get around permissions bug, even with sudo it seems to have an issue
-				echo $i | sudo tee /sys/class/leds/$1/trigger >/dev/null
-				echo "${1} set to respond to event ${i}" 
+				echo "${trigger}" | sudo tee /sys/class/leds/"${1}"/trigger >/dev/null
+				echo "${1} set to respond to event ${trigger}" 
 			fi
 			((option++))
 		done
 		quitnum=${#event_array[@]}
-		if [[ $CHOICE = $quitnum ]] ; then
-			break
+		if [[ "${CHOICE}" = "${quitnum}" ]] ; then
+			CHOICE=-1
+			QUIT_TRIGGER_MENU=1
 		fi
 		
 	done
@@ -65,7 +66,7 @@ led_menu(){
 	QUIT_LED_MENU=0 #exit variable
 	while [ $QUIT_LED_MENU -eq 0 ] ; do 
 		echo ""
-		echo $1 #print name of led
+		echo "${1}" #print name of led
 		echo "========================"
 		echo "What would you like to do with this LED:"
 		echo "1) Turn on"
@@ -73,21 +74,22 @@ led_menu(){
 		echo "3) Associate with a system event"
 		echo "4) Quit"
 		echo "Please enter a number for your choice:"
-		read CHOICE
-		if [[ $CHOICE = 1 ]] ; then
-			echo "1" >/sys/class/leds/$1/brightness #turn light on
+		read -r CHOICE
+		if [[ "${CHOICE}" = 1 ]] ; then
+			echo "1" >/sys/class/leds/"${1}"/brightness #turn light on
 		fi
 
-		if [[ $CHOICE = 2 ]] ; then
-			echo "0" >/sys/class/leds/$1/brightness #turn it off
+		if [[ "${CHOICE}" = 2 ]] ; then
+			echo "0" >/sys/class/leds/"${1}"/brightness #turn it off
 		fi
 
-		if [[ $CHOICE = 3 ]] ; then
-			trigger_menu $1 #enter trigger menu
+		if [[ "${CHOICE}" = 3 ]] ; then
+			trigger_menu "${1}" #enter trigger menu
 			CHOICE=-1
 		fi
 
-		if [[ $CHOICE = 4 ]] ; then
+		if [[ "${CHOICE}" = 4 ]] ; then
+			CHOICE=-1
 			QUIT_LED_MENU=1
 		fi
 	done
@@ -98,9 +100,9 @@ led_menu(){
 #get all the LED dirs in /sys/class/leds/
 
 for dir in /sys/class/leds/*/; do
-  		a=(${dir#"/sys/class/leds/"}) #remove directory prefix
-  		a=(${a%"/"}) #remove directory suffix
-  		LEDLIST+=($a) #add to LEDLIST array
+  		a=("${dir#"/sys/class/leds/"}") #remove directory prefix
+  		a=("${a%"/"}") #remove directory suffix
+  		LEDLIST+=("${a[0]}") #add to LEDLIST array
 	done
 
 
@@ -108,17 +110,17 @@ for dir in /sys/class/leds/*/; do
 while [ $QUIT -eq 0 ] ; do
 	print_menu "${LEDLIST[@]}"
 	
-	read CHOICE #get user input for menu
+	read -r CHOICE #get user input for menu
 
-	for i in ${!LEDLIST[@]}; do
-		if [[ $i = $CHOICE ]] ; then
-			led_menu ${LEDLIST[$i]}
+	for lednumber in "${!LEDLIST[@]}"; do
+		if [[ $lednumber = "${CHOICE}" ]] ; then
+			led_menu "${LEDLIST[$lednumber]}"
 			CHOICE=-1
 		fi
 	done
 
 	quitnum=${#LEDLIST[@]}
-	if [[ $CHOICE = $quitnum ]] ; then
-		QUIT=1
+	if [[ "${CHOICE}" = "${quitnum}" ]] ; then
+		QUIT=1 #exit program
 	fi
 done
